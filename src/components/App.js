bb4879a25ea93ac3ff494455cfc1e897933d6301
Mark Nuiver <mark@vrijblikken.nl>
2016-11-28 15:38:48 +0100

    Prizes assigned to top three

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,21 @@ const playerData = [
     avatar: 'http://cd.sseu.re/matthew-32094823098.png',
     name: 'Matthew',
     points: 0,
+    prize: ' ',
   },
   {
     playerId: 2,
     avatar: 'http://cd.sseu.re/miriam-3409830948.png',
     name: 'Miriam',
     points: 0,
+    prize: ' ',
   },
   {
     playerId: 3,
     avatar: 'http://cd.sseu.re/wouter-38249879847.png',
     name: 'Wouter',
     points: 0,
+    prize: ' ',
   }
 ]
 
@@ -42,8 +45,10 @@ class App extends React.Component {
       return player
     })
 
+    var orderedPlayers = this.orderPlayers(newPlayers)
+
     this.setState({
-      players: this.orderPlayers(newPlayers)
+      players: this.assignPrizes(orderedPlayers)
     })
   }
 
@@ -51,6 +56,25 @@ class App extends React.Component {
     return players.concat().sort((a, b) => {
       return b.points - a.points
     })
+  }
+
+  assignPrizes(players) {
+    const newPlayers = players.map((player) => {
+
+      if ( players.indexOf(player) == 0 && player.points > 9 ) {
+        return Object.assign({}, player, { prize: 'Gold' })
+      }
+      else if ( players.indexOf(player) == 1 && player.points > 9 ) {
+        return Object.assign({}, player, { prize: 'Silver' })
+      }
+      else if ( players.indexOf(player) == 2 && player.points > 9 ) {
+        return Object.assign({}, player, { prize: 'Bronze' })
+      }
+      else {
+        return player
+      }
+    })
+    return newPlayers
   }
 
   render() {
